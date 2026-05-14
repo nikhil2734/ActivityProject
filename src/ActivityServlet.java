936a14a3d5d46a75e740585d2c324be8911f4cf0
Bhavana <bhavanagn13@gmail.com>
2026-05-15 00:12:57 +0530

    Update ActivityServlet.java

--- src/ActivityServlet.java
+++ src/ActivityServlet.java
@@ -16,7 +16,7 @@ public class ActivityServlet extends HttpServlet {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection con = DriverManager.getConnection("jdbc:mysql://activity-db:3306/activitydb", "root", "nikhil@1234");
+            Connection con = DriverManager.getConnection("jdbc:mysql://activity-db:3306/activitydb", "root", "password123");
 
             PreparedStatement ps = con.prepareStatement(
                     "INSERT INTO activity_log (username, action_performed) VALUES (?, ?)");
